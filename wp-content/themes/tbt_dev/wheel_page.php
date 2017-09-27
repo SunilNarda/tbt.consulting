2dae3e04d164bbae2d2309e3f86d0c4dc3aec8ad
Andreas Hagen <me@andreashagen.at>
2017-09-27 11:42:57 +0200

    Kreis mittig zentriert

--- wp-content/themes/tbt_dev/wheel_page.php
+++ wp-content/themes/tbt_dev/wheel_page.php
@@ -5,21 +5,28 @@
 
 <?php get_template_part( 'navigation'); ?>
 
-<div id="wheel_wrapper">
-    <div class="wheel_part1" id="wheelie1">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/cause.png" alt="CAUSE">
-      </div>
-      <div class="wheel_part2" id="wheelie2">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/clarity.png" alt="CLARITY">
-      </div>
-      <div class="wheel_part3" id="wheelie3">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/commitment.png" alt="COMMITMENT">
-      </div>
-      <div class="wheel_part4" id="wheelie4">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/corage.png" alt="COURAGE">
-      </div>
-</div>
+<style>
+#wheel_wrapper{
+  margin-top:500px;
+}
+</style>
 
+<div id="wheel_container">
+  <div id="wheel_wrapper">
+      <div class="wheel_part1" id="wheelie1">
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/cause.png" alt="CAUSE">
+        </div>
+        <div class="wheel_part2" id="wheelie2">
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/clarity.png" alt="CLARITY">
+        </div>
+        <div class="wheel_part3" id="wheelie3">
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/commitment.png" alt="COMMITMENT">
+        </div>
+        <div class="wheel_part4" id="wheelie4">
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/corage.png" alt="COURAGE">
+        </div>
+  </div>
+</div>
 
 <div id="content_wrapper">
 <?php if ( have_posts() ) : ?>
