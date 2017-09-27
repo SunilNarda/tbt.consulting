4327d25e41eb0ae153cb48c0653840fa692a37f4
Andreas Hagen <me@andreashagen.at>
2017-09-27 14:33:13 +0200

    Textfelder sind jetzt über ACF befüllbar

--- wp-content/themes/tbt_dev/wheel_page.php
+++ wp-content/themes/tbt_dev/wheel_page.php
@@ -6,40 +6,50 @@
 <?php get_template_part( 'navigation'); ?>
 
 <style>
-#wheel_wrapper{
-  margin-top:500px;
-}
+  #wheel_wrapper {
+    margin-top: 500px;
+  }
 </style>
 
 <div id="wheel_container">
   <div id="wheel_wrapper">
-      <div id="wheel_part1">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/cause.png" alt="CAUSE">
-        </div>
-        <div id="wheel_part2">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/clarity.png" alt="CLARITY">
-        </div>
-        <div id="wheel_part3">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/commitment.png" alt="COMMITMENT">
-        </div>
-        <div id="wheel_part4">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/corage.png" alt="COURAGE">
-        </div>
+    <div id="wheel_part1">
+      <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/cause.png" alt="CAUSE">
+    </div>
+    <div id="wheel_part2">
+      <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/clarity.png" alt="CLARITY">
+    </div>
+    <div id="wheel_part3">
+      <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/commitment.png" alt="COMMITMENT">
+    </div>
+    <div id="wheel_part4">
+      <img src="<?php bloginfo('stylesheet_directory'); ?>/images/animation/corage.png" alt="COURAGE">
+    </div>
   </div>
 </div>
 
-<div id="cause_textfield" class="wheel_textfield">Cause</div>
-<div id="clarity_textfield" class="wheel_textfield">Clarity</div>
-<div id="commitment_textfield" class="wheel_textfield">Commitment</div>
-<div id="courage_textfield" class="wheel_textfield">Courage</div>
 
+<div id="wheel_textfelder">
+  <div id="cause_textfield" class="wheel_textfield">
+    <?php the_field("cause_txt"); ?>
+  </div>
+  <div id="clarity_textfield" class="wheel_textfield">
+    <?php the_field("clarity_txt"); ?>
+  </div>
+  <div id="commitment_textfield" class="wheel_textfield">
+    <?php the_field("commitment_txt"); ?>
+  </div>
+  <div id="courage_textfield" class="wheel_textfield">
+    <?php the_field("courage_txt"); ?>
+  </div>
+</div>
 
 <div id="content_wrapper">
-<?php if ( have_posts() ) : ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-        <? the_content(); ?>
+  <?php if ( have_posts() ) : ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+  <? the_content(); ?>
     <?php endwhile; ?>
-<?php endif; ?>
+    <?php endif; ?>
 </div>
 
 
